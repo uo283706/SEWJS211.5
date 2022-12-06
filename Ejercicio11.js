f84bc3d3c1d4da82090f88fc40de6dc5827bbf07
uo283706 <113984425+uo283706@users.noreply.github.com>
2022-12-07 00:31:04 +0100

    Add files via upload

---
+++ Ejercicio11.js
@@ -0,0 +1,52 @@
+class Geolocalizacion{
+    constructor (){
+    
+    } 
+
+
+    initMap(){
+
+        var centro = {lat: 43.3672702, lng: -5.8502461};
+        var mapaGeoposicionado = new google.maps.Map(document.getElementById('mapa'),{
+            zoom: 8,
+            center:centro,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        });
+        
+        var infoWindow = new google.maps.InfoWindow;
+        if (navigator.geolocation) {
+              navigator.geolocation.getCurrentPosition(function(position) {
+                var pos = {
+                  lat: position.coords.latitude,
+                  lng: position.coords.longitude
+                };
+    
+                infoWindow.setPosition(pos);
+                infoWindow.setContent('Localización encontrada');
+                infoWindow.open(mapaGeoposicionado);
+                mapaGeoposicionado.setCenter(pos);
+              }, function() {
+                handleLocationError(true, infoWindow, mapaGeoposicionado.getCenter());
+              });
+            } else {
+              // Browser doesn't support Geolocation
+              handleLocationError(false, infoWindow, mapaGeoposicionado.getCenter());
+            }
+
+    }
+
+    handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: Ha fallado la geolocalización' :
+                              'Error: Su navegador no soporta geolocalización');
+        infoWindow.open(mapaGeoposicionado);
+    }
+
+}
+ 
+var mapa = new Object();
+
+var localizacion = new Geolocalizacion();
+
+mapa.initMap = localizacion.initMap;
